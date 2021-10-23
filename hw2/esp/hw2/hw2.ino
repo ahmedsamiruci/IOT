57f99fee8573f9a0131a45d10d9d18062cb81e45
ahmedsamiruci <ahmedas2@uci.edu>
2021-10-23 16:22:10 -0700

    adding Timers to ESP example

--- hw2/esp/hw2/hw2.ino
+++ hw2/esp/hw2/hw2.ino
@@ -4,37 +4,110 @@
 extern "C" {
 #include "user_interface.h"
 }
-
-const char* ssid = "Ahmed Google";
-const char* pswd = "P@ssw0rd01062602900";
-
-const char* hostIP = "192.168.86.99";
-const uint16_t hostTcpPort = 10000;
+#include "ESP8266TimerInterrupt.h"
+#include "ESP8266_ISR_Timer.h"
 
 
-void setupWiFi(void){
+#define LED_TOGGLE_INTERVAL_MS        2000L
+
+
+WiFiClient client;
+ESP8266Timer ITimer;
+ESP8266_ISR_Timer ISR_Timer;
+
+volatile uint32_t startMillis = 0;
+
+void blinkLed(void);
+
+
+void IRAM_ATTR TimerHandler()
+{
+#if USING_ESP32_S2_TIMER_INTERRUPT
+  /////////////////////////////////////////////////////////
+  // Always call this for ESP32-S2 before processing ISR
+  TIMER_ISR_START(timerNo);
+  /////////////////////////////////////////////////////////
+#endif
+  static int timeRun  = 0;
+
+  ISR_Timer.run();
+
+  //Serial.printf("ISR!\n");
+  // Toggle LED every LED_TOGGLE_INTERVAL_MS = 2000ms = 2s
+  /*if (++timeRun == ((LED_TOGGLE_INTERVAL_MS) / HW_TIMER_INTERVAL_US) )
+    {
+    timeRun = 0;
+
+    //timer interrupt toggles pin LED_BUILTIN
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
+    }*/
+
+#if USING_ESP32_S2_TIMER_INTERRUPT
+  /////////////////////////////////////////////////////////
+  // Always call this for ESP32-S2 after processing ISR
+  TIMER_ISR_END(timerNo);
+  /////////////////////////////////////////////////////////
+#endif
+}
+
+void setupWiFi(void) {
 
   WiFi.begin(ssid, pswd);
 
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
+    digitalWrite(initLED, !digitalRead(initLED));
+    delay(200);
     Serial.print(".");
   }
   Serial.println();
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
+
+  digitalWrite(initLED, LOW);
+}
+
+void doSomething(uint8_t indx) {
+  Serial.printf("Hello Something: %d\n", indx);
+}
+void doSomething1(void) {
+  doSomething(1);
+}
+void doSomething2(void) {
+  doSomething(2);
+}
+void doSomething5(void) {
+  doSomething(5);
+}
+
+void initTimers(void) {
+  // Interval in microsecs
+  if (ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_US, TimerHandler))
+  {
+    startMillis = millis();
+    Serial.print(F("Starting ITimer OK, millis() = ")); Serial.println(startMillis);
+  }
+  else
+    Serial.println(F("Can't set ITimer. Select another freq. or timer"));
+
+
+  ISR_Timer.setInterval(1000, doSomething1);
+  ISR_Timer.setInterval(2000, doSomething2);
+  ISR_Timer.setInterval(5000, doSomething5);
 }
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-   
+  while (!Serial);
+  delay(100);
+
   Serial.printf("SDK version: %s \n", system_get_sdk_version());
-  
+
   // Setup pins
 
   // Setup LED pins
@@ -47,27 +120,40 @@ void setup() {
   pinMode(blueLED, OUTPUT);
   digitalWrite(blueLED, LOW);
 
+  initTimers();
+
   Serial.println("Initialization Complete\n");
 
-  setupWiFi();
+  blinkLed();
 
+  // setupWiFi();
 }
 
-WiFiClient client;
 
-void loop() {
-  // put your main code here, to run repeatedly:
+unsigned int readSensor() {
+  int sensorVal = 0;
 
-  Serial.printf("Connection status: %d\n",client.connected());
+#ifdef MOCK_SENSOR
+  sensorVal = random(200);
+#else
+  sensorVal = analogRead(photoSensor);
+#endif
+
+  Serial.printf("New Sensor Value = %d\n", sensorVal);
+  return sensorVal;
+}
+
+void tcpConnection(void) {
+  Serial.printf("Connection status: %d\n", client.connected());
   Serial.printf("Connecting to (%s):%d\n", hostIP, hostTcpPort);
 
-  if(0 == client.connected()){
-    if(!client.connect(hostIP, hostTcpPort)){
+  if (0 == client.connected()) {
+    if (!client.connect(hostIP, hostTcpPort)) {
       Serial.printf("Connection failed\nWait for 5 sec...\n");
       delay(5000);
       return;
     }
-    else{
+    else {
       Serial.printf("Connected to Server with local port: %d\n", client.localPort());
     }
   }
@@ -75,8 +161,8 @@ void loop() {
   // This will send the request to the server
   client.println("hello from ESP8266");
 
-  Serial.printf("Connection status: %d\n",client.connected());
-  
+  Serial.printf("Connection status: %d\n", client.connected());
+
   //read back one line from server
   Serial.println("receiving from remote server");
   //String line = client.readStringUntil('!');
@@ -84,7 +170,7 @@ void loop() {
 
   //Serial.println("closing connection");
   //client.stop();
-  
+
   //Serial.println("wait 5 sec...");
   //delay(5000);
 
@@ -101,4 +187,22 @@ void loop() {
 
   Serial.println("wait 5 sec...");
   delay(5000);
+}
+
+
+void blinkLed(void) {
+
+  for ( uint8 i = 0; i < 10; i++)
+  {
+    digitalWrite(initLED, !digitalRead(initLED));
+    delay(200);
+  }
+
+  digitalWrite(initLED, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
 }
--- hw2/esp/hw2/program_defines.h
+++ hw2/esp/hw2/program_defines.h
@@ -9,10 +9,13 @@ const uint8 blueLED = D3;
 const uint8 photoSensor = A0;                 // Photo Sensor has to be connected to analog pin
 const uint8 pushBtn = D6;
 
+const uint8 initLED = redLED;
 
 static const int readingIntMS = 200;           // Sensor reading interval (milliseconds)
 static const int calibrationIntMS = 3000;       // time needed for calibration
 static const int readTimeMS = 2000;      
+static const int blinkingTimerMS = 250;
+static const int blinkingWindowMS = 250;  
 
 static const int lightDebounce = 30;
 
@@ -21,6 +24,14 @@ static const int lightDebounce = 30;
 #define interrupt_trigger_type            RISING // interrupt triggered on a RISING input
 #define debounce                              10 // time to wait in milli secs
 
+
+const char* ssid = "Ahmed Google";
+const char* pswd = "P@ssw0rd01062602900";
+
+const char* hostIP = "192.168.86.99";
+const uint16_t hostTcpPort = 10000;
+
+#define HW_TIMER_INTERVAL_US      10000L
 
 //#define MOCK_SENSOR
 
