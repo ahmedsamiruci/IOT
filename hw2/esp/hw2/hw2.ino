50ca681e3f1d48b1d7d451a968ed687a17a35786
ahmedsamiruci <ahmedas2@uci.edu>
2021-10-24 09:14:01 -0700

    connection behavior complete

--- hw2/esp/hw2/hw2.ino
+++ hw2/esp/hw2/hw2.ino
@@ -6,19 +6,27 @@ extern "C" {
 }
 #include "ESP8266TimerInterrupt.h"
 #include "ESP8266_ISR_Timer.h"
-
-
 #define LED_TOGGLE_INTERVAL_MS        2000L
+
+
+bool connectToServer(void);
+
+typedef enum {
+  BLINKY_MODE_UNDEF = 0,
+  BLINKY_MODE_COUNTED,
+  BLINKY_MODE_INFINITY,
+} enu_blinkTimerMode;
 
 
 WiFiClient client;
 ESP8266Timer ITimer;
 ESP8266_ISR_Timer ISR_Timer;
+volatile uint32_t blinkyTimerId;
+volatile uint32_t blinkCount = 0;
+volatile enu_blinkTimerMode blinkMode = BLINKY_MODE_UNDEF;
+volatile bool bRetryConn = false;
 
 volatile uint32_t startMillis = 0;
-
-void blinkLed(void);
-
 
 void IRAM_ATTR TimerHandler()
 {
@@ -28,20 +36,8 @@ void IRAM_ATTR TimerHandler()
   TIMER_ISR_START(timerNo);
   /////////////////////////////////////////////////////////
 #endif
-  static int timeRun  = 0;
 
   ISR_Timer.run();
-
-  //Serial.printf("ISR!\n");
-  // Toggle LED every LED_TOGGLE_INTERVAL_MS = 2000ms = 2s
-  /*if (++timeRun == ((LED_TOGGLE_INTERVAL_MS) / HW_TIMER_INTERVAL_US) )
-    {
-    timeRun = 0;
-
-    //timer interrupt toggles pin LED_BUILTIN
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-
-    }*/
 
 #if USING_ESP32_S2_TIMER_INTERRUPT
   /////////////////////////////////////////////////////////
@@ -51,36 +47,71 @@ void IRAM_ATTR TimerHandler()
 #endif
 }
 
+void startBlinkyTimer(enu_blinkTimerMode mode, uint32_t count)
+{
+  blinkMode = mode;
+  blinkCount = count;
+  ISR_Timer.enable(blinkyTimerId);
+}
+
+void stopBlinkyTimer(void) {
+  blinkMode = BLINKY_MODE_INFINITY;
+  blinkCount = 0;
+  ISR_Timer.disable(blinkyTimerId);
+}
+
 void setupWiFi(void) {
 
   WiFi.begin(ssid, pswd);
 
-  Serial.print("Connecting");
+  startBlinkyTimer(BLINKY_MODE_INFINITY, 0);
+
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED)
   {
-    digitalWrite(initLED, !digitalRead(initLED));
-    delay(200);
+    delay(500);
     Serial.print(".");
   }
   Serial.println();
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
-
-  digitalWrite(initLED, LOW);
 }
 
-void doSomething(uint8_t indx) {
-  Serial.printf("Hello Something: %d\n", indx);
-}
-void doSomething1(void) {
-  doSomething(1);
-}
-void doSomething2(void) {
-  doSomething(2);
-}
-void doSomething5(void) {
-  doSomething(5);
+
+void IRAM_ATTR blinkLedCb(void) {
+  static int32_t count = 0;
+
+  switch ( blinkMode ) {
+    case BLINKY_MODE_COUNTED:
+      {
+        count++;
+        if ((blinkCount - count) >= 0) {
+          digitalWrite(initLED, !digitalRead(initLED));
+        }
+        else {
+          // Turn off LED
+          digitalWrite(initLED, LOW);
+          // reset the counters
+          count = 0;
+          blinkCount = 0;
+          //stop timer
+          ISR_Timer.disable(blinkyTimerId);
+        }
+        break;
+      }
+
+    case BLINKY_MODE_INFINITY:
+      {
+        digitalWrite(initLED, !digitalRead(initLED));
+        break;
+      }
+
+    default:
+      Serial.printf("Error in starting the timer\n");
+      //stop timer
+      ISR_Timer.disable(blinkyTimerId);
+  }
 }
 
 void initTimers(void) {
@@ -94,11 +125,14 @@ void initTimers(void) {
     Serial.println(F("Can't set ITimer. Select another freq. or timer"));
 
 
-  ISR_Timer.setInterval(1000, doSomething1);
-  ISR_Timer.setInterval(2000, doSomething2);
-  ISR_Timer.setInterval(5000, doSomething5);
+  blinkyTimerId = ISR_Timer.setInterval(200, blinkLedCb);
+  ISR_Timer.disable(blinkyTimerId);
 }
 
+
+void tryConnectionCb(){
+  bRetryConn = true;
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -109,7 +143,6 @@ void setup() {
   Serial.printf("SDK version: %s \n", system_get_sdk_version());
 
   // Setup pins
-
   // Setup LED pins
   pinMode(redLED, OUTPUT);
   digitalWrite(redLED, LOW);
@@ -124,9 +157,10 @@ void setup() {
 
   Serial.println("Initialization Complete\n");
 
-  blinkLed();
+  setupWiFi();
 
-  // setupWiFi();
+  connectToServer();
+ 
 }
 
 
@@ -143,66 +177,70 @@ unsigned int readSensor() {
   return sensorVal;
 }
 
-void tcpConnection(void) {
+bool connectToServer(void) {
   Serial.printf("Connection status: %d\n", client.connected());
   Serial.printf("Connecting to (%s):%d\n", hostIP, hostTcpPort);
 
   if (0 == client.connected()) {
     if (!client.connect(hostIP, hostTcpPort)) {
-      Serial.printf("Connection failed\nWait for 5 sec...\n");
-      delay(5000);
-      return;
+      Serial.printf("Connection failed, Wait for 5 sec then connect again\n");
+      ISR_Timer.setTimeout(5000, tryConnectionCb);
+      startBlinkyTimer(BLINKY_MODE_INFINITY, 0);
+      return false;
     }
     else {
-      Serial.printf("Connected to Server with local port: %d\n", client.localPort());
+      Serial.printf("Connected to Server {local port: %d}\n", client.localPort());
+      //stop the timer
+      stopBlinkyTimer();
+      digitalWrite(initLED, LOW);
+      return true;
     }
   }
-
-  // This will send the request to the server
-  client.println("hello from ESP8266");
-
-  Serial.printf("Connection status: %d\n", client.connected());
-
-  //read back one line from server
-  Serial.println("receiving from remote server");
-  //String line = client.readStringUntil('!');
-  //Serial.println(line);
-
-  //Serial.println("closing connection");
-  //client.stop();
-
-  //Serial.println("wait 5 sec...");
-  //delay(5000);
-
-  while (client.available() == 0);
-
-  Serial.println("Data Received...");
-
-  while (client.available()) {
-    char ch = static_cast<char>(client.read());
-    Serial.print(ch);
-  }
-
-  Serial.println();
-
-  Serial.println("wait 5 sec...");
-  delay(5000);
 }
 
+void sendTcpData(void)
+{
 
-void blinkLed(void) {
+  /*
+    // This will send the request to the server
+    client.println("hello from ESP8266");
 
-  for ( uint8 i = 0; i < 10; i++)
-  {
-    digitalWrite(initLED, !digitalRead(initLED));
-    delay(200);
-  }
+    Serial.printf("Connection status: %d\n", client.connected());
 
-  digitalWrite(initLED, LOW);
+    //read back one line from server
+    Serial.println("receiving from remote server");
+    //String line = client.readStringUntil('!');
+    //Serial.println(line);
+
+    //Serial.println("closing connection");
+    //client.stop();
+
+    //Serial.println("wait 5 sec...");
+    //delay(5000);
+
+    while (client.available() == 0);
+
+    Serial.println("Data Received...");
+
+    while (client.available()) {
+      char ch = static_cast<char>(client.read());
+      Serial.print(ch);
+    }
+
+    Serial.println();
+
+    Serial.println("wait 5 sec...");
+    delay(5000);
+
+  */
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-
+  if(bRetryConn)
+  {
+    bRetryConn = false;
+    connectToServer();
+  }
 }
